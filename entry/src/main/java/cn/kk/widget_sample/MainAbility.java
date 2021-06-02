db9382474d5412bb5d6eda6158194674d5fa0dfb
felix <lovejiuwei@gmail.com>
2021-06-02 19:03:57 +0800

    #felix + card onclick

--- entry/src/main/java/cn/kk/widget_sample/MainAbility.java
+++ entry/src/main/java/cn/kk/widget_sample/MainAbility.java
@@ -3,31 +3,44 @@ package cn.kk.widget_sample;
 import cn.kk.widget_sample.io.MyDataHelper;
 import cn.kk.widget_sample.slice.MainAbilitySlice;
 import cn.kk.widget_sample.utils.ImageUtil;
+import cn.kk.widget_sample.utils.LogUtil;
 import cn.kk.widget_sample.utils.TimeUtil;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.ability.ProviderFormInfo;
 import ohos.aafwk.content.Intent;
+import ohos.aafwk.content.Operation;
 import ohos.agp.components.ComponentProvider;
-import ohos.media.image.ImageSource;
-import ohos.media.image.PixelMap;
+import ohos.event.intentagent.IntentAgent;
+import ohos.event.intentagent.IntentAgentConstant;
+import ohos.event.intentagent.IntentAgentHelper;
+import ohos.event.intentagent.IntentAgentInfo;
+
+import java.util.*;
 
 /**
- *
  * 1. 创建卡片
  * 2. 被动更新卡片
  * 3. 删除卡片
  */
 public class MainAbility extends Ability {
+
     private static final int DEFAULT_DIMENSION_2X2 = 2;
     // 这个是在创建卡片时，对话框中输入的，在创建之后 config.json 里面 forms -> name
     private static final String CARD_NAME = "service_widget";
     // 是否有数据
-    private boolean hasData = true;
+    private final boolean hasData = true;
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setMainRoute(MainAbilitySlice.class.getName());
+
+        // 如果杀死进程再进入是可以获取的。
+        if (intent.hasParameter(AbilitySlice.PARAM_FORM_IDENTITY_KEY)) {
+            long formId = intent.getLongParam(AbilitySlice.PARAM_FORM_IDENTITY_KEY, 0);
+            LogUtil.info("HM", "MainAbility.formId" + formId);
+        }
     }
 
     @Override
@@ -38,36 +51,61 @@ public class MainAbility extends Ability {
 
         // 获取服务卡片 ID，这里获取的是 int 类型。long 类型也可以(华为技术支持说的)
         int formId = intent.getIntParam(AbilitySlice.PARAM_FORM_ID_KEY, -1);
+//        int R = intent.getIntParam(AbilitySlice.PARAM_FORM_CUSTOMIZE_KEY, -1);
+//        int R = intent.getIntParam(AbilitySlice.PARAM_FORM_DIMENSION_KEY, -1);
+//        int R = intent.getIntParam(AbilitySlice.PARAM_FORM_HEIGHT_KEY, -1);
+//        int R = intent.getIntParam(AbilitySlice.PARAM_FORM_IDENTITY_KEY, -1);
 
         if (formName.equals(CARD_NAME)) {
             // 2 x 2 卡片
             if (dimension == DEFAULT_DIMENSION_2X2) {
 
                 // 保存卡片 id
-                MyDataHelper.saveCardId(formId,getContext());
+                MyDataHelper.saveCardId(formId, getContext());
 
                 // Layout_form_image_with_info_service_widget_2_2 是服务卡片的布局
                 ProviderFormInfo providerFormInfo = new ProviderFormInfo(ResourceTable.Layout_form_image_with_info_service_widget_2_2, this);
 
                 // 如果创建的时候，就有数据要放到卡片上，那么调用如下代码
                 if (hasData) {
-                    ComponentProvider provider = new ComponentProvider(ResourceTable.Layout_form_image_with_info_service_widget_2_2,getContext());
-
+                    ComponentProvider provider = new ComponentProvider(ResourceTable.Layout_form_image_with_info_service_widget_2_2, getContext());
                     // 文字描述
-                    provider.setText(ResourceTable.Id_tv_card_detail,"快来一起学鸿蒙吧!");
-                    // 更新图片
-
-                    // 点击控制事件
-
-
+                    provider.setText(ResourceTable.Id_tv_card_detail, "快来一起学鸿蒙吧!");
+                    // 官网文档，照抄点击控制事件
+                    provider.setIntentAgent(ResourceTable.Id_tv_card_detail, startAbilityIntentAgent());
                     // 填充数据到卡片
                     providerFormInfo.mergeActions(provider);
                 }
-
                 return providerFormInfo;
             }
         }
         return null;
+    }
+
+    // 设置触发的事件为系统预置的HarmonyOS betaApp应用
+    private IntentAgent startAbilityIntentAgent() {
+        Intent intent = new Intent();
+        Operation operation = new Intent.OperationBuilder()
+                // 除了以下这2个，其余的方法都试过了，均跳不了，不知道怎么回事。
+                .withBundleName("cn.kk.widget_sample")
+                .withAbilityName("cn.kk.widget_sample.TestAbility")
+                .build();
+        intent.setOperation(operation);
+        intent.setParam("test", "RRR");
+        List<Intent> intentList = new ArrayList<>();
+        intentList.add(intent);
+        List<IntentAgentConstant.Flags> flags = new ArrayList<>();
+        flags.add(IntentAgentConstant.Flags.UPDATE_PRESENT_FLAG);
+        IntentAgentInfo paramsInfo = new IntentAgentInfo(200, IntentAgentConstant.OperationType.START_ABILITY, flags, intentList, null);
+        return IntentAgentHelper.getIntentAgent(this, paramsInfo);
+    }
+
+    @Override
+    protected void onCastTempForm(long formId) {
+        // 使用方将临时卡片转换为常态卡片触发，提供方需要做相应的处理
+        super.onCastTempForm(formId);
+
+        LogUtil.info("HM", "onCastTempForm");
     }
 
     @Override
@@ -78,9 +116,8 @@ public class MainAbility extends Ability {
         // [Java 卡片开发指导](https://developer.harmonyos.com/cn/docs/documentation/doc-guides/ability-service-widget-provider-java-0000001104082220)
 
         ComponentProvider componentProvider = new ComponentProvider(ResourceTable.Layout_form_image_with_info_service_widget_2_2, getContext());
-        componentProvider.setImagePixelMap(ResourceTable.Id_iv_card, ImageUtil.getPixelMap(ResourceTable.Media_bg_android,getContext()));
+        componentProvider.setImagePixelMap(ResourceTable.Id_iv_card, ImageUtil.getPixelMap(ResourceTable.Media_bg_android, getContext()));
         componentProvider.setText(ResourceTable.Id_tv_card_detail, "被动更新了，" + TimeUtil.getTime());
-
     }
 
     @Override
@@ -92,4 +129,36 @@ public class MainAbility extends Ability {
             MyDataHelper.deleteCardId(getContext());
         }
     }
+
+    @Override
+    protected void onEventNotify(Map<Long, Integer> formEvents) {
+        super.onEventNotify(formEvents);
+
+        LogUtil.info("HM", "onEventNotify," + formEvents);
+    }
+
+    public void onForeground(Intent intent) {
+        // onResume
+        LogUtil.info("HM", "onForeground");
+    }
+
+    public void onActive() {
+        // 在 onForeground 之后
+        LogUtil.info("HM", "onActive");
+    }
+
+    public void onInactive() {
+        // onPause
+        LogUtil.info("HM", "onInactive");
+    }
+
+    public void onBackground() {
+        // 在 onInactive 之后
+        LogUtil.info("HM", "onBackground");
+    }
+
+    public void onStop() {
+        LogUtil.info("HM", "onStop");
+    }
+
 }
--- entry/src/main/java/cn/kk/widget_sample/slice/MainAbilitySlice.java
+++ entry/src/main/java/cn/kk/widget_sample/slice/MainAbilitySlice.java
@@ -3,10 +3,10 @@ package cn.kk.widget_sample.slice;
 import cn.kk.widget_sample.ResourceTable;
 import cn.kk.widget_sample.io.MyDataHelper;
 import cn.kk.widget_sample.utils.ImageUtil;
+import cn.kk.widget_sample.utils.LogUtil;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.ability.FormException;
 import ohos.aafwk.content.Intent;
-import ohos.agp.components.Component;
 import ohos.agp.components.ComponentProvider;
 
 public class MainAbilitySlice extends AbilitySlice {
@@ -18,9 +18,8 @@ public class MainAbilitySlice extends AbilitySlice {
         findComponentById(ResourceTable.Id_btnUpdate).setClickedListener(component -> {
 
             ComponentProvider componentProvider = new ComponentProvider(ResourceTable.Layout_form_image_with_info_service_widget_2_2, getContext());
-            componentProvider.setText(ResourceTable.Id_tv_card_detail, "手动更新了");
-            componentProvider.setImagePixelMap(ResourceTable.Id_iv_card, ImageUtil.getPixelMap(ResourceTable.Media_bg_android,getContext()));
-
+            componentProvider.setText(ResourceTable.Id_tv_card_detail, "主动更新了");
+            componentProvider.setImagePixelMap(ResourceTable.Id_iv_card, ImageUtil.getPixelMap(ResourceTable.Media_bg_android, getContext()));
 
             long smallWidgetFormId = MyDataHelper.getCardId(getContext());
             if (smallWidgetFormId != -1) {
@@ -30,22 +29,31 @@ public class MainAbilitySlice extends AbilitySlice {
                     e.printStackTrace();
                 }
             }
-
-            try {
-                getAbility().updateForm(smallWidgetFormId,componentProvider);
-            } catch (FormException e) {
-                e.printStackTrace();
-            }
         });
     }
 
-    @Override
-    public void onActive() {
-        super.onActive();
+    public void onForeground(Intent intent) {
+        // onResume
+        LogUtil.info("HM", "onForeground");
     }
 
-    @Override
-    public void onForeground(Intent intent) {
-        super.onForeground(intent);
+    public void onActive() {
+        // 在 onForeground 之后
+        LogUtil.info("HM", "onActive");
     }
+
+    public void onInactive() {
+        // onPause
+        LogUtil.info("HM", "onInactive");
+    }
+
+    public void onBackground() {
+        // 在 onInactive 之后
+        LogUtil.info("HM", "onBackground");
+    }
+
+    public void onStop() {
+        LogUtil.info("HM", "onStop");
+    }
+
 }
